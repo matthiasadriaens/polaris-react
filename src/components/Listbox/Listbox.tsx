ab87c5dc97ba6672d381f4b332765cd3a6c1e9d8
Lo Kim <lo.kim@shopify.com>
2022-03-16 20:30:22 -0400

    [Listbox] Fix logic to reset and find next valid option on create
    Fixes logic that wasn't setting create option as active when creating a new option that has no match to existing listbox options. Also fixes logic that wasn't setting next active option if current active option was the first element in listbox when keying up after creating a new option.
    
    Also adds new logic to new reset active option when onScrollToBottom passed in and invoked.

--- src/components/Listbox/Listbox.tsx
+++ src/components/Listbox/Listbox.tsx
@@ -227,6 +227,14 @@ export function Listbox({
         // reset when number of listbox children change
         if (nextListLength !== length) {
           reset = true;
+
+          // do not reset if onKeyToBottom passed in and navItems updating
+          const navItems = getNavigableOptions();
+          if (onKeyToBottom && navItems.length === length + 1) {
+            reset = false;
+          }
+
+          setReset(reset);
           return nextListLength;
         }
 
@@ -234,10 +242,16 @@ export function Listbox({
       });
 
       if (reset) {
-        setReset(true);
+        setReset(reset);
       }
     }
-  }, [children, listLength, getFirstNavigableOption, activeOption]);
+  }, [
+    children,
+    listLength,
+    getFirstNavigableOption,
+    activeOption,
+    onKeyToBottom,
+  ]);
 
   useEffect(() => {
     if (loading || listLength <= 1) return;
@@ -337,7 +351,8 @@ export function Listbox({
           if (key === 'down') {
             currentIndex = currentIndex === lastIndex ? 0 : currentIndex + 1;
           } else {
-            currentIndex--;
+            currentIndex =
+              nextIndex === lastIndex ? nextIndex : currentIndex - 1;
           }
 
           // restart while loop to find next valid option
